1e6fea31049f05bd35ee90631e3250f9e419a692
hund555 <allanholmiversen@gmail.com>
2022-04-29 13:18:14 +0200

    tilføjet Master og Slave
    init

---
+++ Mega2560Master/Mega2560Master/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 29-04-2022 13:00:26
+ * Author : alla1862
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
