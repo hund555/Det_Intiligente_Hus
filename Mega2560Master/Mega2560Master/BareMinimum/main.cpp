8a3a2e7ea311d9b9acdaddb7624a5ecfa3a2e45a
hund555 <allanholmiversen@gmail.com>
2022-05-02 10:17:31 +0200

    Added display librarys and made code for display to work. added clock library

--- Mega2560Master/Mega2560Master/BareMinimum/main.cpp
+++ Mega2560Master/Mega2560Master/BareMinimum/main.cpp
@@ -1,24 +1,70 @@
 ﻿#include <Arduino.h>
 #include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// Display
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+// clock
+
+bool showTime = false;
+
+// delays
+unsigned long previousMillis2 = 0;
+int delay2Sec = 2000;
 
 void setup (void)
 {
-	Serial.begin(115200);				//set baud rate to 115200 for usart
+	Serial.begin(115200);												//set baud rate to 115200 for usart
 	SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));	// Set configuration for SPI
-	SPI.begin();						// Initializes the SPI bus by setting SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high.
+	SPI.begin();														// Initializes the SPI bus by setting SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high.
+	
+	if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+	{ // Address for 128x64
+		Serial.println(F("SSD1306 allocation failed"));
+		for(;;); // Don't proceed, loop forever
+	}
 }
 
 void loop (void)
 {
-	digitalWrite(SS, LOW);				// enable Slave Select
+	display.clearDisplay();
 	
-	// send test string
-	for (const char* p = "Hello, world!\r" ; *p != '\0'; p++)
+	display.setTextSize(1);			// Normal 1:1 pixel scale
+	display.setTextColor(WHITE);	// Draw white text
+	display.setCursor(0,0);			// Start at top-left corner
+	if (showTime)
 	{
-		SPI.transfer (*p);
-		Serial.print(*p);
+	}
+	else
+	{
+		if (millis() - previousMillis2 >= delay2Sec)
+		{
+			previousMillis2 = millis();
+			digitalWrite(SS, LOW);				// enable Slave Select
+			
+			// send test string
+			int* temperature = 0;
+			SPI.transfer(*temperature);
+			Serial.println(*temperature);
+			
+			digitalWrite(SS, HIGH);				// disable Slave Select
+			
+			String dstr = "Temp: ";				//dstr = Display show temperature rounded
+			dstr += *temperature;
+			dstr += (char)247;
+			
+			display.println(dstr);
+		}
 	}
 	
-	digitalWrite(SS, HIGH);				// disable Slave Select
-	delay(2000);
+	display.display();
 }
