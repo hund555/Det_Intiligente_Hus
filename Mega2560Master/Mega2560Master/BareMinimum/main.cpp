9ade567b61925cc96c33d271077063603e9e864a
hund555 <allanholmiversen@gmail.com>
2022-05-02 09:32:57 +0200

    Added code for Master from example

--- Mega2560Master/Mega2560Master/BareMinimum/main.cpp
+++ Mega2560Master/Mega2560Master/BareMinimum/main.cpp
@@ -1,12 +1,24 @@
 ﻿#include <Arduino.h>
+#include <SPI.h>
 
-
-void setup() {
-  // put your setup code here, to run once:
-
+void setup (void)
+{
+	Serial.begin(115200);				//set baud rate to 115200 for usart
+	SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE0));	// Set configuration for SPI
+	SPI.begin();						// Initializes the SPI bus by setting SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop (void)
+{
+	digitalWrite(SS, LOW);				// enable Slave Select
+	
+	// send test string
+	for (const char* p = "Hello, world!\r" ; *p != '\0'; p++)
+	{
+		SPI.transfer (*p);
+		Serial.print(*p);
+	}
+	
+	digitalWrite(SS, HIGH);				// disable Slave Select
+	delay(2000);
 }
